8dd06680ed0525fc12b89d12b3ca2147e78f53a0
desbma-s1n <desbma-s1n@users.noreply.github.com>
2023-09-22 16:41:51 +0200

    feat: improve summary code to do a single hashmap search + support some more syscalls

--- src/summarize.rs
+++ src/summarize.rs
@@ -27,22 +27,35 @@ pub enum ProgramAction {
     Syscalls(HashSet<String>),
 }
 
-struct OpenSyscallInfo {
-    relfd_idx: Option<usize>,
-    path_idx: usize,
-    flags_idx: usize,
-}
-
-struct RenameSyscallInfo {
-    relfd_src_idx: Option<usize>,
-    path_src_idx: usize,
-    relfd_dst_idx: Option<usize>,
-    path_dst_idx: usize,
-    flags_idx: Option<usize>,
-}
-
-struct MmapSyscallInfo {
-    prot_idx: usize,
+/// Meta structure to group syscalls that have similar summary handling
+/// and store argument indexes
+enum SyscallInfo {
+    Mmap {
+        prot_idx: usize,
+    },
+    Network {
+        sockaddr_idx: usize,
+    },
+    Open {
+        relfd_idx: Option<usize>,
+        path_idx: usize,
+        flags_idx: usize,
+    },
+    Rename {
+        relfd_src_idx: Option<usize>,
+        path_src_idx: usize,
+        relfd_dst_idx: Option<usize>,
+        path_dst_idx: usize,
+        flags_idx: Option<usize>,
+    },
+    Socket,
+    StatFd {
+        fd_idx: usize,
+    },
+    StatPath {
+        relfd_idx: Option<usize>,
+        path_idx: usize,
+    },
 }
 
 lazy_static! {
@@ -52,87 +65,82 @@ lazy_static! {
     // - https://filippo.io/linux-syscall-table/
     // - https://linasm.sourceforge.net/docs/syscalls/filesystem.php
     //
-    static ref OPEN_SYSCALL: HashMap<&'static str, OpenSyscallInfo> = HashMap::from([
+    static ref SYSCALL_MAP: HashMap<&'static str, SyscallInfo> = HashMap::from([
+        // mmap
+        ("mmap", SyscallInfo::Mmap { prot_idx: 2 }),
+        ("mmap2", SyscallInfo::Mmap { prot_idx: 2 }),
+        ("shmat", SyscallInfo::Mmap { prot_idx: 2 }),
+        ("mprotect", SyscallInfo::Mmap { prot_idx: 2 }),
+        ("pkey_mprotect", SyscallInfo::Mmap { prot_idx: 2 }),
+
+        // network
+        ("connect", SyscallInfo::Network { sockaddr_idx: 1 }),
+        ("bind", SyscallInfo::Network { sockaddr_idx: 1 }),
+        ("recvfrom", SyscallInfo::Network { sockaddr_idx: 4 }),
+        ("sendto", SyscallInfo::Network { sockaddr_idx: 4 }),
+        // TODO recvmsg/sendmsg
+
+        // open
         (
             "open",
-            OpenSyscallInfo {
+            SyscallInfo::Open {
                 relfd_idx: None,
                 path_idx: 0,
-                flags_idx: 1
-            }
+                flags_idx: 1,
+            },
         ),
         (
             "openat",
-            OpenSyscallInfo {
+            SyscallInfo::Open {
                 relfd_idx: Some(0),
                 path_idx: 1,
-                flags_idx: 2
-            }
-        )
-    ]);
-    static ref RENAME_SYSCALL: HashMap<&'static str, RenameSyscallInfo> = HashMap::from([
+                flags_idx: 2,
+            },
+        ),
+
+        // rename
         (
             "rename",
-            RenameSyscallInfo {
+            SyscallInfo::Rename {
                 relfd_src_idx: None,
                 path_src_idx: 0,
                 relfd_dst_idx: None,
                 path_dst_idx: 1,
                 flags_idx: None,
-            }
+            },
         ),
         (
             "renameat",
-            RenameSyscallInfo {
+            SyscallInfo::Rename {
                 relfd_src_idx: Some(0),
                 path_src_idx: 1,
                 relfd_dst_idx: Some(2),
                 path_dst_idx: 3,
                 flags_idx: None,
-            }
+            },
         ),
         (
             "renameat2",
-            RenameSyscallInfo {
+            SyscallInfo::Rename {
                 relfd_src_idx: Some(0),
                 path_src_idx: 1,
                 relfd_dst_idx: Some(2),
                 path_dst_idx: 3,
                 flags_idx: Some(4),
-            }
-        )
-    ]);
-    static ref MMAP_SYSCALL: HashMap<&'static str, MmapSyscallInfo> = HashMap::from([
-        (
-            "mmap",
-            MmapSyscallInfo {
-                prot_idx: 2,
-            }
+            },
         ),
-        (
-            "mmap2",
-            MmapSyscallInfo {
-                prot_idx: 2,
-            }
-        ),
-        (
-            "shmat",
-            MmapSyscallInfo {
-                prot_idx: 2,
-            }
-        ),
-        (
-            "mprotect",
-            MmapSyscallInfo {
-                prot_idx: 2,
-            }
-        ),
-        (
-            "pkey_mprotect",
-            MmapSyscallInfo {
-                prot_idx: 2,
-            }
-        ),
+
+        // socket
+        ("socket", SyscallInfo::Socket),
+
+        // stat fd
+        ("fstat", SyscallInfo::StatFd { fd_idx: 0 }),
+        ("getdents", SyscallInfo::StatFd { fd_idx: 0 }),
+
+        // stat path
+        ("stat", SyscallInfo::StatPath { relfd_idx: None, path_idx: 0 }),
+        ("lstat", SyscallInfo::StatPath { relfd_idx: None, path_idx: 0 }),
+        ("newfstatat", SyscallInfo::StatPath { relfd_idx: Some(0), path_idx: 1 }),
     ]);
 }
 
@@ -200,173 +208,192 @@ where
             .or_insert(1);
         let name = syscall.name.as_str();
 
-        // TODO convert into a single HashMap search
-        if let Some(open_info) = OPEN_SYSCALL.get(name) {
-            let (mut path, flags) = if let (
-                Some(SyscallArg::Buffer {
-                    value: b,
-                    type_: BufferType::Unknown,
-                }),
-                Some(SyscallArg::Integer { value: e, .. }),
-            ) = (
-                syscall.args.get(open_info.path_idx),
-                syscall.args.get(open_info.flags_idx),
-            ) {
-                (PathBuf::from(OsStr::from_bytes(b)), e)
-            } else {
-                anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
-            };
-
-            path = if let Some(path) = resolve_path(&path, open_info.relfd_idx, &syscall) {
-                path
-            } else {
-                continue;
-            };
-
-            if flags.is_flag_set("O_CREAT") {
-                actions.push(ProgramAction::Create(path.clone()));
-            }
-            if flags.is_flag_set("O_WRONLY")
-                || flags.is_flag_set("O_RDWR")
-                || flags.is_flag_set("O_TRUNC")
-            {
-                actions.push(ProgramAction::Write(path.clone()));
-            }
-            if !flags.is_flag_set("O_WRONLY") {
-                actions.push(ProgramAction::Read(path));
-            }
-        } else if let Some(rename_info) = RENAME_SYSCALL.get(name) {
-            let (mut path_src, mut path_dst) = if let (
-                Some(SyscallArg::Buffer {
-                    value: b1,
-                    type_: BufferType::Unknown,
-                }),
-                Some(SyscallArg::Buffer {
-                    value: b2,
-                    type_: BufferType::Unknown,
-                }),
-            ) = (
-                syscall.args.get(rename_info.path_src_idx),
-                syscall.args.get(rename_info.path_dst_idx),
-            ) {
-                (
-                    PathBuf::from(OsStr::from_bytes(b1)),
-                    PathBuf::from(OsStr::from_bytes(b2)),
-                )
-            } else {
-                anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
-            };
-
-            (path_src, path_dst) = if let (Some(path_src), Some(path_dst)) = (
-                resolve_path(&path_src, rename_info.relfd_src_idx, &syscall),
-                resolve_path(&path_dst, rename_info.relfd_dst_idx, &syscall),
-            ) {
-                (path_src, path_dst)
-            } else {
-                continue;
-            };
-
-            let exchange = if let Some(flags_idx) = rename_info.flags_idx {
-                let flags = if let Some(SyscallArg::Integer { value: flags, .. }) =
-                    syscall.args.get(flags_idx)
+        match SYSCALL_MAP.get(name) {
+            Some(SyscallInfo::Open {
+                relfd_idx,
+                path_idx,
+                flags_idx,
+            }) => {
+                let (mut path, flags) = if let (
+                    Some(SyscallArg::Buffer {
+                        value: b,
+                        type_: BufferType::Unknown,
+                    }),
+                    Some(SyscallArg::Integer { value: e, .. }),
+                ) =
+                    (syscall.args.get(*path_idx), syscall.args.get(*flags_idx))
                 {
-                    flags
+                    (PathBuf::from(OsStr::from_bytes(b)), e)
                 } else {
                     anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
                 };
 
-                flags.is_flag_set("RENAME_EXCHANGE")
-            } else {
-                false
-            };
+                path = if let Some(path) = resolve_path(&path, *relfd_idx, &syscall) {
+                    path
+                } else {
+                    continue;
+                };
 
-            actions.push(ProgramAction::Read(path_src.clone()));
-            actions.push(ProgramAction::Write(path_src.clone()));
-            if exchange {
-                actions.push(ProgramAction::Read(path_dst.clone()));
-            } else {
-                actions.push(ProgramAction::Create(path_dst.clone()));
+                if flags.is_flag_set("O_CREAT") {
+                    actions.push(ProgramAction::Create(path.clone()));
+                }
+                if flags.is_flag_set("O_WRONLY")
+                    || flags.is_flag_set("O_RDWR")
+                    || flags.is_flag_set("O_TRUNC")
+                {
+                    actions.push(ProgramAction::Write(path.clone()));
+                }
+                if !flags.is_flag_set("O_WRONLY") {
+                    actions.push(ProgramAction::Read(path));
+                }
             }
-            actions.push(ProgramAction::Write(path_dst.clone()));
-        } else if name.starts_with("getdents") {
-            // TODO factorize with newfstatat, handle other stat variants?
-            let mut path = syscall
-                .args
-                .get(0)
-                .and_then(|a| a.metadata())
-                .map(|m| PathBuf::from(OsStr::from_bytes(m)))
-                .ok_or_else(|| anyhow::anyhow!("Unexpected args for {name}"))?;
-            path = if let Some(path) = resolve_path(&path, None, &syscall) {
-                path
-            } else {
-                continue;
-            };
-            actions.push(ProgramAction::Read(path));
-        } else if name.starts_with("newfstatat") {
-            let mut path = if let Some(SyscallArg::Buffer {
-                value: b,
-                type_: BufferType::Unknown,
-            }) = syscall.args.get(1)
-            {
-                PathBuf::from(OsStr::from_bytes(b))
-            } else {
-                anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
-            };
-            path = if let Some(path) = resolve_path(&path, Some(0), &syscall) {
-                path
-            } else {
-                continue;
-            };
-            actions.push(ProgramAction::Read(path));
-        } else if ["bind", "connect"].contains(&name) {
-            // TODO other network syscalls that can handle UDS (sendto, sendmsg, recvfrom, recvmsg)
+            Some(SyscallInfo::Rename {
+                relfd_src_idx,
+                path_src_idx,
+                relfd_dst_idx,
+                path_dst_idx,
+                flags_idx,
+            }) => {
+                let (mut path_src, mut path_dst) = if let (
+                    Some(SyscallArg::Buffer {
+                        value: b1,
+                        type_: BufferType::Unknown,
+                    }),
+                    Some(SyscallArg::Buffer {
+                        value: b2,
+                        type_: BufferType::Unknown,
+                    }),
+                ) = (
+                    syscall.args.get(*path_src_idx),
+                    syscall.args.get(*path_dst_idx),
+                ) {
+                    (
+                        PathBuf::from(OsStr::from_bytes(b1)),
+                        PathBuf::from(OsStr::from_bytes(b2)),
+                    )
+                } else {
+                    anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
+                };
 
-            let (af, addr) = if let Some(SyscallArg::Struct(members)) = syscall.args.get(1) {
+                (path_src, path_dst) = if let (Some(path_src), Some(path_dst)) = (
+                    resolve_path(&path_src, *relfd_src_idx, &syscall),
+                    resolve_path(&path_dst, *relfd_dst_idx, &syscall),
+                ) {
+                    (path_src, path_dst)
+                } else {
+                    continue;
+                };
+
+                let exchange = if let Some(flags_idx) = flags_idx {
+                    let flags = if let Some(SyscallArg::Integer { value: flags, .. }) =
+                        syscall.args.get(*flags_idx)
+                    {
+                        flags
+                    } else {
+                        anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
+                    };
+
+                    flags.is_flag_set("RENAME_EXCHANGE")
+                } else {
+                    false
+                };
+
+                actions.push(ProgramAction::Read(path_src.clone()));
+                actions.push(ProgramAction::Write(path_src.clone()));
+                if exchange {
+                    actions.push(ProgramAction::Read(path_dst.clone()));
+                } else {
+                    actions.push(ProgramAction::Create(path_dst.clone()));
+                }
+                actions.push(ProgramAction::Write(path_dst.clone()));
+            }
+            Some(SyscallInfo::StatFd { fd_idx }) => {
+                let mut path = syscall
+                    .args
+                    .get(*fd_idx)
+                    .and_then(|a| a.metadata())
+                    .map(|m| PathBuf::from(OsStr::from_bytes(m)))
+                    .ok_or_else(|| anyhow::anyhow!("Unexpected args for {name}"))?;
+                path = if let Some(path) = resolve_path(&path, None, &syscall) {
+                    path
+                } else {
+                    continue;
+                };
+                actions.push(ProgramAction::Read(path));
+            }
+            Some(SyscallInfo::StatPath {
+                relfd_idx,
+                path_idx,
+            }) => {
+                let mut path = if let Some(SyscallArg::Buffer {
+                    value: b,
+                    type_: BufferType::Unknown,
+                }) = syscall.args.get(*path_idx)
+                {
+                    PathBuf::from(OsStr::from_bytes(b))
+                } else {
+                    anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
+                };
+                path = if let Some(path) = resolve_path(&path, *relfd_idx, &syscall) {
+                    path
+                } else {
+                    continue;
+                };
+                actions.push(ProgramAction::Read(path));
+            }
+            Some(SyscallInfo::Network { sockaddr_idx }) => {
+                let (af, addr) =
+                    if let Some(SyscallArg::Struct(members)) = syscall.args.get(*sockaddr_idx) {
+                        let af = if let Some(SyscallArg::Integer {
+                            value: IntegerExpression::NamedConst(af),
+                            ..
+                        }) = members.get("sa_family")
+                        {
+                            af
+                        } else {
+                            anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
+                        };
+                        (af.as_str(), members)
+                    } else {
+                        // Can be NULL in some cases, ie AF_NETLINK sockets
+                        continue;
+                    };
+
+                #[allow(clippy::single_match)]
+                match af {
+                    "AF_UNIX" => {
+                        if let Some(path) = socket_address_uds_path(addr, &syscall) {
+                            actions.push(ProgramAction::Read(path));
+                        };
+                    }
+                    _ => (),
+                }
+            }
+            Some(SyscallInfo::Socket) => {
                 let af = if let Some(SyscallArg::Integer {
                     value: IntegerExpression::NamedConst(af),
                     ..
-                }) = members.get("sa_family")
+                }) = syscall.args.get(0)
                 {
-                    af
+                    af.to_string()
                 } else {
                     anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
                 };
-                (af.as_str(), members)
-            } else {
-                anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
-            };
-
-            #[allow(clippy::single_match)]
-            match af {
-                "AF_UNIX" => {
-                    if let Some(path) = socket_address_uds_path(addr, &syscall) {
-                        actions.push(ProgramAction::Read(path));
-                    };
+                actions.push(ProgramAction::NetworkActivity { af });
+            }
+            Some(SyscallInfo::Mmap { prot_idx }) => {
+                let prot = if let Some(SyscallArg::Integer { value: prot, .. }) =
+                    syscall.args.get(*prot_idx)
+                {
+                    prot
+                } else {
+                    anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
+                };
+                if prot.is_flag_set("PROT_WRITE") && prot.is_flag_set("PROT_EXEC") {
+                    actions.push(ProgramAction::WxMemoryMapping);
                 }
-                _ => (),
             }
-        } else if name == "socket" {
-            let af = if let Some(SyscallArg::Integer {
-                value: IntegerExpression::NamedConst(af),
-                ..
-            }) = syscall.args.get(0)
-            {
-                af.to_string()
-            } else {
-                anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
-            };
-            actions.push(ProgramAction::NetworkActivity { af });
-        } else if let Some(mmap_info) = MMAP_SYSCALL.get(name) {
-            let prot = if let Some(SyscallArg::Integer { value: prot, .. }) =
-                syscall.args.get(mmap_info.prot_idx)
-            {
-                prot
-            } else {
-                anyhow::bail!("Unexpected args for {}: {:?}", name, syscall.args);
-            };
-            if prot.is_flag_set("PROT_WRITE") && prot.is_flag_set("PROT_EXEC") {
-                actions.push(ProgramAction::WxMemoryMapping);
-            }
+            None => (),
         }
     }
 
